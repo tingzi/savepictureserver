46973023738484aff8ab301bd14ef88f82883226
amy.zhang <amy.zhang@dongmancorp.cn>
2018-05-30 19:00:00 +0800

    first commit

---
+++ src/main/java/com/quick/savepictureserver/SavepictureserverApplication.java
@@ -0,0 +1,13 @@
+package com.quick.savepictureserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavepictureserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavepictureserverApplication.class, args);
+	}
+}
